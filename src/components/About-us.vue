f9406bae95f4c167d83d482eda3dd948e2a11bfd
Bakhtiyar Tayirov <bakhtiyarkhozha@gmail.com>
2018-11-07 18:41:57 +0500

    Initial commit

---
+++ src/components/About-us.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <router-link to='/'>Home page</router-link>
+    <router-link to='/products/'>Products page</router-link>
+    <div class="content">
+      About us
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'About-us'
+  }
+</script>
+<style scoped>
+
+</style>
