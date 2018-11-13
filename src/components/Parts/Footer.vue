f993fbbffb1f9cd6c1f5918d64f16cf3d473cf7f
Bakhtiyar Tayirov <bakhtiyarkhozha@gmail.com>
2018-11-13 14:37:05 +0500

    added style footer form

--- src/components/Parts/Footer.vue
+++ src/components/Parts/Footer.vue
@@ -329,4 +329,239 @@
       }
     }
   }
+
+  .region-footer {
+    background: #353535;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    #newsletter{
+      max-width: 1470px;
+      width: 100%;
+      margin: auto;
+      display: flex;
+      justify-content: space-between;
+      font-family: 'Circe Bold';
+      .left{
+        display: flex;
+        align-items: center;
+        .signup{
+          display: flex;
+          font-size: 18px;
+          img{
+            margin-right: 20px;
+          }
+          p{
+            color: #fff;
+          }
+        }
+        .form{
+          margin-left: 50px;
+          background: #fff;
+          border-radius: 4px;
+          .submit_panel{
+            height: 30px;
+            width: 400px;
+            background: #fff;
+            border: none;
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+            padding: 0 20px;
+            box-sizing: border-box;
+            font-family: 'Circe Regular';
+            &:hover{
+              cursor: text;
+            }
+          }
+          .submit_button{
+            height: 50px;
+            width: 100px;
+            background: $orange;
+            border: none;
+            border-top-right-radius: 4px;
+            border-bottom-right-radius: 4px;
+            text-transform: uppercase;
+            color: #fff;
+            font-family: 'Circe Bold';
+            font-size: 14px;
+            &:hover{
+              cursor: pointer;
+            }
+          }
+        }
+      }
+      #socials{
+        display: flex;
+        align-items: center;
+        border-left: 1px solid #535353;
+        padding-left: 45px;
+        span{
+          color: #fff;
+          margin-right: 15px;
+        }
+        a{
+          background: #fff;
+          margin-left: 5px;
+          border-radius: 50%;
+          i{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 40px;
+            width: 40px;
+            font-size: 15px;
+            &:hover{
+              transition: .4s;
+              color: #fff;
+              cursor: pointer;
+            }
+          }
+          &:hover{
+            background: $orange;
+            transition: .4s;
+          }
+        }
+      }
+    }
+  }
+  @include b(1200px, max-width){
+    .region-footer{
+      min-height: 170px;
+      #newsletter{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        align-items: center;
+        .left{
+          @include span(12);
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          margin-bottom: 15px;
+          margin-right: 0;
+          .signup{
+            margin-bottom: 10px;
+            margin-right: 30px;
+          }
+          .form{
+            margin-bottom: 10px;
+            margin-left: 0;
+            max-height: 50px;
+          }
+        }
+        #socials{
+          border: 0;
+          span{
+            margin-left: 10px;
+          }
+          a{
+            margin-left: 7px;
+          }
+        }
+      }
+    }
+  }
+  @include b(992px, max-width){
+    #partners{
+      margin-top: 0;
+      padding-top: 20px;
+    }
+    #description{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      div{
+        @include span(13.8);
+        margin: 0;
+        padding-top: 25px;
+        margin-bottom: -25px;
+        &:nth-child(2){
+          border: none;
+        }
+      }
+    }
+    .region-footer{
+      #newsletter{
+        .left{
+          .form{
+            .submit_panel{
+              width: 300px;
+            }
+          }
+        }
+      }
+    }
+  }
+  @include b(768px, max-width) {
+    #partners{
+      max-width: 100%;
+      .owl-nav{
+        display: none;
+      }
+    }
+    .region-footer{
+      #newsletter{
+        .left{
+          .form{
+            .submit_panel{
+              width: 200px;
+            }.submit_button{
+               width: 70px;
+             }
+          }
+        }
+      }
+    }
+  }
+  @include b(540px, max-width) {
+    #partners{
+      .owl-carousel{
+        display: flex;
+        height: 150px;
+        .owl-nav{
+          display: none;
+        }
+      }
+    }
+    .region-footer{
+      min-height: 200px;
+      #newsletter{
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        .left{
+          display: flex;
+          flex-direction: column;
+
+          .signup{
+            margin-bottom: 10px;
+          }
+          .form{
+            margin-bottom: 10px;
+            margin-left: 0;
+            max-height: 50px;
+            .submit_panel{
+              max-width: 240px;
+            }
+            .submit_button{
+            }
+          }
+        }
+        #socials{
+          padding-left: 0;
+          margin-right: 10px;
+          span{
+            font-size: 16px;
+            width: 70px;
+            margin-right: 0;
+          }
+          a{
+            margin-left: 7px;
+          }
+        }
+      }
+    }
+  }
+
+
 </style>
